9d52b40e86679e6dde85d05c13347aff419a6753
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-04 10:32:07 +0530

    uc5 added

--- sorting.sh
+++ sorting.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
-read -p "Enter your first number" a
-read -p "Enter your second number" b
-read -p "Enter your third number" c
+read -p "Enter your first number: " a
+read -p "Enter your second number: " b
+read -p "Enter your third number: " c
 
 var=$(( $a+$b*$c ))
 var2=$(( $a*$b+$c ))
 var3=$(( $c+$a/$b ))
+var4=$(( $a%$b+$c ))
